5ef3a4b2c23778941cbf13dea802b4b094f8538b
masoudkazak <kzmasut@gmail.com>
2022-11-18 00:10:56 +0330

    docker

---
+++ volumes/app/sib_cofe/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("api/", include("food.urls")),
+    path("api/accounts/", include("account.urls")),
+    path('api-auth/', include('rest_framework.urls'))
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
